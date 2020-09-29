207c53828ed04918be255e4e373474c4a77fe02b
jurij <yura19802007@gmail.com>
2020-09-29 17:10:30 +0300

    Change Blabla to About

--- app.rb
+++ app.rb
@@ -7,3 +7,14 @@ get '/' do
 	
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
+get '/about' do
+
+	erb :about
+end
+
+get '/contacts' do 
+
+	erb :contacts
+
+end
