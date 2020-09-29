2ab44cb42c1c3b87b984a935553aae1d2b1dce1d
jurij <yura19802007@gmail.com>
2020-09-29 18:34:10 +0300

    Add contact and visit

--- app.rb
+++ app.rb
@@ -19,7 +19,34 @@ get '/contacts' do
 
 end
 
+post '/contacts' do
+
+	@user_name = params[:user_name]
+	@user_email = params[:user_email]
+	@subject = params[:subject]
+	@user_message = params[:user_message]
+
+	erb :contact
+
+	f = File.open './contacts', 'a'
+	f.write "name: #{@user_name};email : #{@user_email}\n#{@subject}\n#{@user_message}"
+	f.close
+
+end
+
 get '/visit' do
 	erb :visit
 end
+
+post '/visit' do
+	@user_name = params[:user_name]
+	@user_phone = params[:user_phone]
+	@date_time = params[:date_time]
+
+	f = File.open './public/users.txt', 'a'
+	f.write "name : #{@user_name}; phone : #{@user_phone}; date : #{@date_time} \n"
+	f.close
+
+end
+
 
